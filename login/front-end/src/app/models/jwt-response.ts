da1fbf3d05d6345a06514c0a54fdb8eff06eb736
juan-jolo-21 <juanlorenzo_800@hotmail.com>
2024-04-03 08:51:56 -0500

    ajuste 1 angular

--- login/front-end/src/app/models/jwt-response.ts
+++ login/front-end/src/app/models/jwt-response.ts
@@ -1,16 +1,18 @@
 export interface JwtResponse {
     dataUser : {
+        id: string,
         user_type: string,
         email: string,
         employee :{
             dni: number,
             person: {
                 name: string,
-                last_name_1: string
+                last_name_1: string,
+                last_name_2: string
             }
             name_branch: string
         }
-        expiresIn: string,
+        expiresIn: number,
         accessToken: string
     }
 
